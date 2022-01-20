94f2d06755766d2f9bfbe1a690245f43720bfed2
Angelica <Angelica137@users.noreply.github.com>
2022-01-20 19:22:42 +0000

    use timedelta to increase seconds

--- scripts/gigasecond.py
+++ scripts/gigasecond.py
@@ -1,9 +1,15 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 bd, bc, be = 2000, 1, 1
 
 birthday = datetime(bd, bc, be)
+gs = timedelta(seconds=+1)
 
 
 def add_gs(birthday: datetime) -> datetime:
     return birthday
+
+
+print(birthday)
+print(gs)
+print(birthday + gs)
--- tests/test_gigasecond.py
+++ tests/test_gigasecond.py
@@ -4,3 +4,10 @@ from scripts.gigasecond import *
 def test_date_returns_input_date():
     bd = (2000, 1, 1)
     assert add_gs(bd) == (2000, 1, 1)
+
+
+'''
+def test_add_gs_returns_input_date():
+    bd = (2000, 1, 1)
+    assert add_gs(bd) == 
+'''
