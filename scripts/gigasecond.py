05a7f5f74b6d9e227852f1fc432307bd6b3f349d
Angelica <Angelica137@users.noreply.github.com>
2022-01-21 17:35:23 +0000

    amend tests to conver bd into datetime

--- scripts/gigasecond.py
+++ scripts/gigasecond.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+
 bd, bc, be = 2000, 1, 1
 
 birthday = datetime(bd, bc, be)
--- tests/test_gigasecond.py
+++ tests/test_gigasecond.py
@@ -7,5 +7,5 @@ def test_date_returns_input_date():
 
 
 def test_add_gs_returns_input_date():
-    bd = (2000, 1, 1)
+    bd = datetime(2000, 1, 1)
     assert add_gs(bd) == (2031, 9, 9, 0, 0)
