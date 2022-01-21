714e8e416a34ec4cbc0064d8155bfa6fb943ffcc
Angelica <Angelica137@users.noreply.github.com>
2022-01-21 18:09:13 +0000

    delete obsolete

--- scripts/gigasecond.py
+++ scripts/gigasecond.py
@@ -1,14 +1,6 @@
 from datetime import datetime, timedelta
 
 
-bd, bc, be = 2000, 1, 1
-
-birthday = datetime(bd, bc, be)
-
-
 def add_gs(birthday: datetime) -> datetime:
     gs = timedelta(seconds=+ (10**9))
     return birthday + gs
-
-
-print(add_gs(birthday))
