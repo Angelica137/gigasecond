1778ddc7f4017c32e4e1e82acab5d543ae2f2f94
Angelica <Angelica137@users.noreply.github.com>
2022-01-21 17:37:01 +0000

    amend tests

--- tests/test_gigasecond.py
+++ tests/test_gigasecond.py
@@ -1,11 +1,6 @@
 from scripts.gigasecond import *
 
 
-def test_date_returns_input_date():
-    bd = (2000, 1, 1)
-    assert add_gs(bd) == (2000, 1, 1, 0, 0)
-
-
 def test_add_gs_returns_input_date():
     bd = datetime(2000, 1, 1)
-    assert add_gs(bd) == (2031, 9, 9, 0, 0)
+    assert add_gs(bd) == datetime(2031, 9, 9, 1, 46, 40)
